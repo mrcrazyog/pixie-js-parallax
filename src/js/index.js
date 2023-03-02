ab4a20f6ca4dc3ff0caf733645a5403362aa35c0
Jan Michalcak <jan@michalcak.eu>
2023-03-02 12:07:12 +0100

    correct parallax

---
+++ src/js/index.js
@@ -0,0 +1,257 @@
+import { Application, Container, TilingSprite } from 'pixi.js';
+import { positionsConfig } from './positionsConfig';
+import * as PIXI from 'pixi.js';
+
+//define absolute path for images
+const basePath = location.href.substring(0, location.href.lastIndexOf('/') + 1);
+
+const sceneSize = {
+  SCENE_WIDTH: 2800,
+  SCENE_HEIGHT: 800,
+};
+
+const stageSize = {
+  STAGE_WIDTH: 1600,
+  STAGE_HEIGHT: 800,
+};
+
+//PIXI initialization =====
+let app = new Application({
+  view: document.getElementById('pixi-canvas'),
+  resolution: window.devicePixelRatio || 1,
+  autoDensity: true,
+  backgroundColor: 0xffffff,
+  width: stageSize.STAGE_WIDTH,
+  height: stageSize.STAGE_HEIGHT,
+});
+
+// Enable interactivity!
+app.stage.interactive = true;
+
+// Make sure the whole canvas area is interactive, not just the circle.
+app.stage.hitArea = app.screen;
+
+//import images
+const bgLayer0Texture = PIXI.Texture.from(basePath + '/assets/l0/bg_0.png');
+const bgLayer1Texture = PIXI.Texture.from(basePath + '/assets/l1/bg_1.png');
+const bgLayer2Texture = PIXI.Texture.from(basePath + '/assets/l2/bg_2.png');
+const bgLayer3Texture = PIXI.Texture.from(basePath + '/assets/l3/bg_3.png');
+const hejkal0 = PIXI.Texture.from(basePath + '/assets/l1/items/hejkal_0.png');
+const hejkal1 = PIXI.Texture.from(basePath + '/assets/l1/items/hejkal_1.png');
+const certi0 = PIXI.Texture.from(
+  basePath + '/assets/l2/items/certi_rodinka_0.png'
+);
+const certi1 = PIXI.Texture.from(
+  basePath + '/assets/l2/items/certi_rodinka_1.png'
+);
+const ded0 = PIXI.Texture.from(basePath + '/assets/l2/items/ded_vseved_0.png');
+const ded1 = PIXI.Texture.from(basePath + '/assets/l2/items/ded_vseved_1.png');
+const jezibaba0 = PIXI.Texture.from(
+  basePath + '/assets/l2/items/jezibaba_0.png'
+);
+const jezibaba1 = PIXI.Texture.from(
+  basePath + '/assets/l2/items/jezibaba_1.png'
+);
+const vodnik0 = PIXI.Texture.from(basePath + '/assets/l2/items/vodnik_0.png');
+const vodnik1 = PIXI.Texture.from(basePath + '/assets/l2/items/vodnik_0.png');
+
+const bgLayer0 = new TilingSprite(
+  bgLayer0Texture,
+  app.screen.width,
+  app.screen.height
+);
+
+const bgLayer1 = new TilingSprite(
+  bgLayer1Texture,
+  app.screen.width,
+  app.screen.height
+);
+
+const bgLayer2 = new TilingSprite(
+  bgLayer2Texture,
+  app.screen.width,
+  app.screen.height
+);
+
+const bgLayer3 = new TilingSprite(
+  bgLayer3Texture,
+  app.screen.width,
+  app.screen.height
+);
+
+//How fast will the layers move? 6:26
+let bgX = 0;
+let currentX = 0;
+
+let bgSpeed = 1;
+let bgMoving = false;
+let bgLayer3Offset = 300;
+
+bgLayer1.x = 0; // Move second layer to the right for a parallax effect
+
+app.stage.addChild(bgLayer0);
+app.stage.addChild(bgLayer1);
+app.stage.addChild(bgLayer2);
+app.stage.addChild(bgLayer3);
+
+const characterSprites = {};
+
+function updateCharactersPosition(bgX) {
+  for (const { items, name } of positionsConfig) {
+    if (items) {
+      // check necessary because not all arrays have the items property
+      for (const { id, x, y } of items) {
+        const character = characterSprites[id];
+
+        if (character) {
+          switch (name) {
+            case 'l1':
+              character.position.set(x + bgLayer1.x - bgX * 0.3, y);
+              break;
+            case 'l2':
+              character.position.set(x + bgLayer2.x - bgX / 2, y);
+              break;
+            case 'l3':
+              character.position.set(x + bgLayer3.x - bgX / 1.5, y);
+              break;
+            default:
+              console.log(`invalid name ${name}`);
+          }
+        }
+      }
+    }
+  }
+}
+
+let previousX = 0;
+
+function updateBg() {
+  if (bgMoving) {
+    bgX += bgSpeed;
+    bgLayer3.tilePosition.x = bgX / 1.5;
+    bgLayer2.tilePosition.x = bgX / 2;
+    bgLayer1.tilePosition.x = bgX / 3;
+    bgLayer0.tilePosition.x = 0;
+  }
+
+  const stopPosition = stageSize.STAGE_WIDTH + 200;
+  const currentX = -bgX;
+
+  if (Math.abs(bgX) >= stopPosition) {
+    bgMoving = false;
+    if (bgX > 0) {
+      bgX = stopPosition;
+    } else {
+      bgX = -stopPosition;
+    }
+    bgLayer3.tilePosition.x = bgX / 1.5;
+    bgLayer2.tilePosition.x = bgX / 2;
+    bgLayer1.tilePosition.x = bgX / 3;
+    bgLayer0.tilePosition.x = 0;
+  }
+
+  const stopPositionLeft = 0.01;
+  if (bgX >= stopPositionLeft) {
+    bgMoving = false;
+    if (bgX > 0) {
+      bgX = stopPositionLeft;
+    } else {
+      bgX = -stopPositionLeft;
+    }
+    bgLayer3.tilePosition.x = bgX / 1.5;
+    bgLayer2.tilePosition.x = bgX / 2;
+    bgLayer1.tilePosition.x = bgX / 3;
+    bgLayer0.tilePosition.x = 0;
+  }
+
+  updateCharactersPosition(currentX, previousX);
+}
+
+//Switch directions
+let lastKeyCode = null;
+
+function switchDirection(e) {
+  if (e.type === 'mousemove') {
+    // Mouse movement
+    const mousePos = { x: e.clientX, y: e.clientY };
+    const stageCenter = app.screen.width / 2;
+    const distanceFromCenter = mousePos.x - stageCenter;
+    const speedFactor = -distanceFromCenter / stageCenter; //negative number necessary for the screen to "follow" the mouse
+    bgSpeed = speedFactor * 10;
+    bgMoving = true;
+  } else if (e.code === 'ArrowLeft' && lastKeyCode !== 'ArrowLeft') {
+    // Left arrow key
+    bgSpeed = 1;
+    bgMoving = true;
+  } else if (e.code === 'ArrowRight' && lastKeyCode !== 'ArrowRight') {
+    // Right arrow key
+    bgSpeed = -1;
+    bgMoving = true;
+  } else if (e.code === 'ArrowLeft' && lastKeyCode === 'ArrowLeft') {
+    bgSpeed += 1;
+  } else if (e.code === 'ArrowRight' && lastKeyCode === 'ArrowRight') {
+    bgSpeed -= 1;
+  }
+
+  lastKeyCode = e.code;
+}
+
+function addCharactersToScene(positionsConfig) {
+  for (const { items, name } of positionsConfig) {
+    if (items) {
+      //check necessary because not all arrays have the items property
+      for (const { id, x, y } of items) {
+        console.log(`The id is ${id} and the name is ${name} `);
+        const charTextureInit = PIXI.Texture.from(
+          basePath + `assets/${name}/items/${id}_0.png`
+        );
+        const character = new PIXI.Sprite(charTextureInit);
+        const charTextureFinal = basePath + `assets/${name}/items/${id}_1.png`;
+        console.log(name);
+
+        // Set the character's position relative to the background layer
+        switch (name) {
+          case 'l1':
+            character.position.set(x + bgLayer1.x, y);
+            bgLayer1.addChild(character);
+            break;
+          case 'l2':
+            character.position.set(x + bgLayer2.x, y);
+            bgLayer2.addChild(character);
+            break;
+          case 'l3':
+            character.position.set(x + bgLayer3.x, y);
+            bgLayer3.addChild(character);
+            break;
+          default:
+            console.log(`invalid name ${name}`);
+        }
+
+        // Add the character sprite to the characterSprites object
+        characterSprites[id] = character;
+        console.log(name, 'name is');
+      }
+    }
+  }
+}
+
+addCharactersToScene(positionsConfig);
+
+document.addEventListener('keydown', (e) => {
+  if (e.code === 'ArrowLeft' || e.code === 'ArrowRight') {
+    bgMoving = true;
+  }
+});
+
+document.addEventListener('keyup', (e) => {
+  if (e.code === 'ArrowLeft' || e.code === 'ArrowRight') {
+    bgMoving = false;
+  }
+});
+
+document.addEventListener('mousemove', switchDirection);
+app.ticker.add(updateBg); // Call updateBg function continuously to update the position of the background layers.
+
+document.addEventListener('mousemove', (e) => {
+  console.log(`Mouse X coordinate: ${e.clientX}`);
+});
---
+++ src/js/positionsConfig.js
@@ -0,0 +1,33 @@
+export const positionsConfig = [
+  {
+    items: [{ id: "drak", x: 1255, y: 434, w: 352, h: 286}],
+    bg: { id: "bg_3", x: -4, y: 0, w: 2804, h: 924 },
+    name: "l3",
+  },
+  {
+    items: [
+      { id: "vodnik", x: 698, y: 411, w: 215, h: 281 },
+      {
+        id: "certi_rodinka",
+        x: 1615,
+        y: 402,
+        w: 325,
+        h: 381
+      },
+      { id: "ded_vseved", x: 2067, y: 393, w: 152, h: 216},
+      { id: "jezibaba", x: 2404, y: 329, w: 244, h: 209},
+    ],
+    bg: { id: "bg_2", x: 135, y: 0, w: 2530, h: 800 },
+    name: "l2",
+  },
+  {
+    items: [
+      { id: "hejkal", x: 1120, y: 282, w: 280, h: 241 },
+    ],
+    bg: { id: "bg_1", x: 341, y: 0, w: 2118, h: 800 },
+    name: "l1",
+  },
+  { 
+    bg: { id: "bg_0", x: 600, y: 0, w: 1601, h: 800 }, 
+    name: "l0" },
+];
